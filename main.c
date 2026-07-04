655fae4be0c4ff0458655d02738753b3c327e830
wnb-hongjh <hongjh124@gmail.com>
2026-07-04 21:26:58 +0900

    연결 리스트 기초 공부

--- main.c
+++ main.c
@@ -1,13 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int value;
+    struct node *next;
+};
 
 int main()
 {
-    float temperature[5] = {23.5, 27.1, 19.8, 31.2, 25.0};
-    float sum = 0;
-    for (int i = 0; i < 5; i++)
+    struct node *a = malloc(sizeof(struct node));
+    struct node *b = malloc(sizeof(struct node));
+    struct node *c = malloc(sizeof(struct node));
+    struct node *d = malloc(sizeof(struct node));
+
+    a->value = 10;
+    b->value = 20;
+    c->value = 30;
+    d->value = 40;
+
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    d->next = NULL;
+
+    struct node *current = a;
+    while (current != NULL)
     {
-        sum += temperature[i];
+        printf("%d\n", current->value);
+        current = current->next;
     }
-    printf("avg: \n", sum / 5);
+
+    free(a);
+    a = NULL;
+    free(b);
+    b = NULL;
+    free(c);
+    c = NULL;
+    free(d);
+    d = NULL;
+
     return 0;
 }
