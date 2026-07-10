bd33601939c2de22be3193af90169c2f556b90ce
wnb-hongjh <hongjh124@gmail.com>
2026-07-11 00:46:18 +0900

    ADC 시뮬레이터 프로젝트 완성

--- main.c
+++ main.c
@@ -1,17 +1,59 @@
+#define _USE_MATH_DEFINES
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+struct Sample
+{
+    int index;
+    float time;
+    float value;
+};
+
+int print_result(FILE *fp)
+{
+    int index;
+    float time, value;
+    while (fscanf(fp, "샘플 #%d  시간: %fs  값: %f", &index, &time, &value) != EOF)
+    {
+        printf("[index]: %d\n[time]: %f\n[value]: %f\n", index, time, value);
+    }
+}
 
 int main()
 {
-    FILE *fp = fopen("sensor.txt", "r");
+    int n = 100;
+    struct Sample *samples = malloc(n * sizeof(struct Sample));
+    if (samples == NULL)
+    {
+        printf("ERROR\n");
+        return 1;
+    }
+    FILE *fp = fopen("sample.txt", "w");
     if (fp == NULL)
     {
-        printf("opening file failed");
+        printf("reading error\n");
         return 1;
     }
 
-    while (fscanf(fp, "ID: %d, 값: %f\n", &id, &value) != EOF)
+    for (int i = 0; i < n; i++)
     {
-        printf("읽어온 데이터 -> ID: %d, 값: %.2f\n", id, value);
+        samples[i].index = i + 1;
+        samples[i].time = i * 0.01;
+        samples[i].value = sin(2 * M_PI * 1.0 * samples[i].time);
+        fprintf(fp, "샘플 #%d  시간: %.2fs  값: %.3f", samples[i].index, samples[i].time, samples[i].value);
+    }
+    fclose(fp);
+    free(samples);
+    samples = NULL;
+
+    char yn;
+    fp = fopen("sample.txt", "r");
+    printf("Do you want to see the result?(y/n): ");
+    scanf("%c", &yn);
+    if (yn == 'y')
+    {
+        print_result(fp);
     }
     fclose(fp);
     return 0;
