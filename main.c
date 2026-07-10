d8c76d692d79813f3d1d1bda4789051dff6618cb
wnb-hongjh <hongjh124@gmail.com>
2026-07-10 23:28:27 +0900

    파일 I/O 공부

--- main.c
+++ main.c
@@ -1,44 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-struct node
-{
-    int value;
-    struct node *next;
-};
 
 int main()
 {
-    struct node *a = malloc(sizeof(struct node));
-    struct node *b = malloc(sizeof(struct node));
-    struct node *c = malloc(sizeof(struct node));
-    struct node *d = malloc(sizeof(struct node));
-
-    a->value = 10;
-    b->value = 20;
-    c->value = 30;
-    d->value = 40;
-
-    a->next = b;
-    b->next = c;
-    c->next = d;
-    d->next = NULL;
-
-    struct node *current = a;
-    while (current != NULL)
+    FILE *fp = fopen("sensor.txt", "r");
+    if (fp == NULL)
     {
-        printf("%d\n", current->value);
-        current = current->next;
+        printf("opening file failed");
+        return 1;
     }
 
-    free(a);
-    a = NULL;
-    free(b);
-    b = NULL;
-    free(c);
-    c = NULL;
-    free(d);
-    d = NULL;
-
+    while (fscanf(fp, "ID: %d, 값: %f\n", &id, &value) != EOF)
+    {
+        printf("읽어온 데이터 -> ID: %d, 값: %.2f\n", id, value);
+    }
+    fclose(fp);
     return 0;
 }
